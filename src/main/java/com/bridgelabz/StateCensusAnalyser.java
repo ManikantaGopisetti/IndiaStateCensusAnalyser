3931b7cd3c372f348c5fe759d0f88023fb90dea7
ManikantaGopisetti <manikantagopisetti2@gmail.com>
2021-12-20 20:19:01 +0530

    Added: Ability for the analyser to load the Indian States Census Information from a csv file

---
+++ src/main/java/com/bridgelabz/StateCensusAnalyser.java
@@ -0,0 +1,33 @@
+package com.bridgelabz;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+
+public class StateCensusAnalyser {
+
+	public int loadIndiaCensusData(String csvFilePath) throws CensusAnalyserException {
+		try {
+			Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
+			CsvToBeanBuilder<CSVStateCensus> csvToBeanBuilder = new CsvToBeanBuilder<CSVStateCensus>(reader);
+			csvToBeanBuilder.withType(CSVStateCensus.class);
+			csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+			CsvToBean<CSVStateCensus> csvToBean = csvToBeanBuilder.build();
+			Iterator<CSVStateCensus> censusCSVIterator = csvToBean.iterator();
+			;
+			int numOfEntries = 0;
+			while (censusCSVIterator.hasNext()) {
+				numOfEntries++;
+				CSVStateCensus censusData = censusCSVIterator.next();
+			}
+			return numOfEntries;
+		} catch (IOException e) {
+			throw new CensusAnalyserException(e.getMessage(),
+					CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+		}
+	}
+}
